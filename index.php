a0188da94de322bb59955426017e4151c3ef9baf
Angelio133 <Angelio006lucas@gmail.com>
2025-01-29 19:25:10 +0100

    Angelio

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="font/font/css/all.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="navbar.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+
+        </div>
+        <div class="maison">
+            
+        </div>
+    </header>
+    <?php
+include_once ("navbar.php");
+    ?>
+    <div class="Ensemble">
+        <h1>Bienvenue dans la gestion administrative</h1>
+        <Section >
+            <div class="haut">
+                <h3>.......</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus consequatur fugiat earum quibusdam, reprehenderit explicabo minima placeat quam culpa delectus sunt quo inventore quaerat iusto iure at, nam nulla?</p>
+                <button>
+                    <a href="#">Ajouter</a>
+                </button>
+            </div>
+            <div class="haut2">
+                <h3>.......</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus consequatur fugiat earum quibusdam, reprehenderit explicabo minima placeat quam culpa delectus sunt quo inventore quaerat iusto iure at, nam nulla?</p>
+                <button>
+                    <a href="#">Modifier</a>
+                </button>
+            </div>
+            <div class="Bas">
+            <h3>.......</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis necessitatibus consequatur fugiat earum quibusdam, reprehenderit explicabo minima placeat quam culpa delectus sunt quo inventore quaerat iusto iure at, nam nulla?</p>
+                <button>
+                    <a href="#">Consulter</a>
+                </button>
+            </div>
+        </Section>
+    </div>
+   
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+<div class="entete">
+    <nav class="navbar">
+        <ul>
+            <li>
+                <a href="acceil"><i class="fa fa-house"></i></i>Acceil</a>
+            </li>
+            <li>
+                <a href="ajouter">Ajouter</a>
+            </li>
+            <li>
+                <a href="modifier">Modifier</a>
+            </li>
+            <li>
+                <a href="consulter">Consulter</a>
+            </li>
+        </ul>
+        <button>
+            <a href="#">Sortir</a>
+        </button>
+    </nav>
+<div>
+</body>
+</html>
