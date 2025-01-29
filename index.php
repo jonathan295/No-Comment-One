6329b792298eae0f0e2c480bea7b82fe368ccb9b
Féru03 <72690924+jonathan295@users.noreply.github.com>
2025-01-29 21:27:45 +0300

    add all

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="" href="font/css/all.css">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="drp_mada">
+                    <img src="" alt="">
+                </div>
+                <h1>IZA IANAO</h1>
+                <div>
+                    <i class="fa fa-user"></i>
+                </div>
+            </nav>
+        </header>
+        <section>
+            <div class="title">
+                <p><span>B</span>inevenue dans la gestion administrative</p>
+            </div>
+            <div class="row">
+                <div class="card">
+                    <div class="titlecard">
+                        <p>Lorem ipsum dolor sit amet</p>
+                    </div>
+                    <div class="paragraphecard">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus est modi officiis esse nihil vitae.</p>
+                    </div>
+                    <a href="" class="btn">Ajouter <i class="fa fa-angle-right"></i></a>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
